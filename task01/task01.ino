b339a7e706916ecf0f00139f97e431b9776be927
ilyataster <taster00@gmail.com>
2022-04-10 22:46:33 +0400

    task 1 init commit

--- task01/task01.ino
+++ task01/task01.ino
@@ -1,7 +1,29 @@
-void setup() {
+int ledPins [] =  {3, 5, 6, 9, 10};
 
+int ledStates [] = {LOW, LOW, LOW, LOW, LOW};
+unsigned long previousMillis [] = {0, 0, 0, 0, 0};
+const long intervals[] = {900000, 333000, 27000, 150, 100};
+
+void setup() {
+  for (int i = 0; i < 5; i++){
+      pinMode(ledPins[i], OUTPUT);
+  }
 }
 
 void loop() {
-
+  for (int i = 0; i < 5; i++){
+    unsigned long currentMillis = micros();
+    if (currentMillis - previousMillis[i] >= intervals[i]) {
+      previousMillis[i] = currentMillis;
+  
+      if (ledStates[i] == LOW) {
+        ledStates[i] = HIGH;
+      } else {
+        ledStates[i] = LOW;
+      }
+  
+      digitalWrite(ledPins[i], ledStates[i]);
+    }
+  }
+ 
 }
